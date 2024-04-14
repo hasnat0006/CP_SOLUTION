4e03dde6b590d2757a178e8dffdde065aa14fe83
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2024-04-15 00:00:02 +0600

    DATE: 15 April 2024

--- temp.cpp
+++ temp.cpp
@@ -1,19 +1,41 @@
-#include <bits/stdc++.h>
+//!-----------------------------------------------------!//
+//!              Author: YUSUF REZA HASNAT              !//
+//!             Created: 14|04|2024 23:25:36            !//
+//!-----------------------------------------------------!//
+
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    int t;
+#define int   long long
+#define float long double
+#define vf(v) (v).begin(), (v).end()
+#define vr(v) (v).rbegin(), (v).rend()
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const int inf = 1e18;
+
+void solve() {
+    string s;
+    cin >> s;
+    cout << s;
+    if(s.size() > 1){
+        s.pop_back();
+        reverse(vf(s));
+        cout << s;
+    }    
+    cout << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int t = 1;
     cin >> t;
-    while (t--) {
-        int n, m, k;
-        cin >> n >> m >> k;
-        int same = n / m;
-        if(n % m != 0)
-            same++;
-        if (n - same <= k)
-            cout << "NO" << endl;
-        else
-            cout << "YES" << endl;
+    for (int i = 1; i <= t; i++){
+        cout << "Case " << i << ": ";
+        solve(); 
     }
     return 0;
 }

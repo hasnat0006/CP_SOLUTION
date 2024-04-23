5dadc4af0cedb04b75d296a1901fb1dd22c2a9a7
hasnat0006 <yusufrezahasnat0006@gmail.com>
2024-04-23 17:41:40 +0600

    DATE: 23 April 2024

--- temp.cpp
+++ temp.cpp
@@ -1,63 +1,67 @@
-// C++ Program to implement
-// the above approach
-#include <bits/stdc++.h>
+//!-----------------------------------------------------!//
+//!              Author: YUSUF REZA HASNAT              !//
+//!             Created: 23|04|2024 17:18:58            !//
+//!-----------------------------------------------------!//
+
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+
 using namespace std;
 
-// Function to print the maximum rooks
-// and their positions
-int pos[1000][1000];
-void countRooks(int n, int k) {
-    int row[n] = {0};
-    int col[n] = {0};
+#define int   long long
+#define float long double
+#define vf(v) (v).begin(), (v).end()
+#define vr(v) (v).rbegin(), (v).rend()
+#define endl "\n"
 
-    // Initialize row and col array
-    for (int i = 0; i < n; i++) {
-        row[i] = 0;
-        col[i] = 0;
-    }
+const int mod = 1e9 + 7;
+const int inf = 1e18;
 
-    // Marking the location of
-    // already placed rooks
-    for (int i = 0; i < k; i++) {
-        row[pos[i][0] - 1] = 1;
-        col[pos[i][1] - 1] = 1;
-    }
+vector<int> color;
+vector<int> adj[200005];
+vector<int> visited;
+vector<int> ans;
 
-    int res = n - k;
-
-    // Print number of non-attacking
-    // rooks that can be placed
-    cout << res << " " << endl;
-
-    // To store the placed rook
-    // location
-    int ri = 0, ci = 0;
-    while (res-- > 0) {
-        // Print lexicographically
-        // smallest order
-        while (row[ri] == 1) {
-            ri++;
+bool dfs(int src, int clr){
+    visited[src] = 1;
+    bool f = true;
+    for(auto i : adj[src]){
+        if(!visited[i]){
+            f &= dfs(i, color[i]);
         }
-        while (col[ci] == 1) {
-            ci++;
-        }
-        cout << (ri + 1) << " " << (ci + 1) << " " << endl;
-        ri++;
-        ci++;
     }
+    cerr << src << " " << clr << " " << f << endl;
+    if(f and (clr == 1 or clr == -1)){
+        ans.push_back(src);
+    }
+    return f;
 }
 
-// Driver Code
-int main() {
-    // Size of board
-    int N;
-    cin >> N;
-    // Number of rooks already placed
-    int K = 0;
-
-    // Position of rooks
-
-    // Function call
-    countRooks(N, K);
+void solve() {
+    int n, k, x;
+    cin >> n >> k;
+    color.resize(n + 1, 0);
+    visited.resize(n + 1, 0);
+    for(int i = 1; i <= k; i++){
+        cin >> x;
+        color[x] = 1;
+    }
+    for(int i = 0; i < n - 1; i++){
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    dfs(color.front(), -1);
+    cout << ans.size() << endl;
 }
-// This code is contributed by shikhasingrajput
+
+int32_t main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++){
+        solve(); 
+    }
+    return 0;
+}

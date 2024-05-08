0428d137df5b0e5417165782ca4e5a5613b810c3
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2024-05-09 00:00:03 +0600

    DATE: 09 May 2024

---
+++ A_Spy_Detected.cpp
@@ -0,0 +1,38 @@
+//!-----------------------------------------------------!//
+//!              Author: YUSUF REZA HASNAT              !//
+//!             Created: 08|05|2024 09:41:14            !//
+//!-----------------------------------------------------!//
+
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define float long double
+#define vf(v) (v).begin(), (v).end()
+#define vr(v) (v).rbegin(), (v).rend()
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const int inf = 1e18;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &x : v)
+        cin >> x;
+    sort(vf(v));
+    cout << (v[0] == v[1] ? v[n - 1] : v[0]) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve();
+    }
+    return 0;
+}

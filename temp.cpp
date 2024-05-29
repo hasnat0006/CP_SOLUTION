71db27f9296d8109b657a51f51dcf9c241889aae
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2024-05-30 00:00:02 +0600

    DATE: 30 May 2024

--- temp.cpp
+++ temp.cpp
@@ -19,19 +19,9 @@ const int inf = 1e18;
 
 
 void solve() {
-    int n;
-    cin >> n;
-    if(n == 1){
-        cout << "No" << endl;
-        return;
-    }
-    for(int i = 2; i * i <= n; i++){
-        if(n % i == 0){
-            cout << "No" << endl;
-            return;
-        }
-    }
-    cout << "Yes" << endl;
+    if(fork() && fork())
+        fork();
+    printf("HELLO");
 }
 
 int32_t main() {

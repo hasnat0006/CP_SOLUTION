f4f8d46e5e0a642228e42cee989488bc661037cb
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2024-06-15 00:00:02 +0600

    DATE: 15 June 2024

--- temp.cpp
+++ temp.cpp
@@ -16,38 +16,31 @@ using namespace std;
 int mod = 1000000007;
 int inf = 1e18;
 
-bool prime[1000000005];
-void sieve(int n = 1000000000) {
-    for (int i = 2; i <= n; i++)
-        prime[i] = 1;
-    for (int i = 4; i <= n; i += 2)
-        prime[i] = 0;
-    for (int i = 3; i * i <= n; i++) {
-        if (prime[i]) {
-            for (int j = i * i; j <= n; j += i * 2)
-                prime[j] = 0;
+void solve() {
+    int n;
+    cin >> n;
+    int temp = 1;
+    set<int> pq;
+    for(int i = 1; i <= n; i++) {
+        pq.insert(i);
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << temp << " ";
+        pq.erase(pq.find(temp));
+        temp *= 2;
+        if(temp > n){
+            temp = *pq.begin();
         }
     }
-    vector<int> primes;
-    for (int i = 2; i <= n; i++) {
-        if (prime[i]) {
-            primes.push_back(i);
-        }
-    }
-    for(auto x: primes)
-        cout << x << " ";
     cout << endl;
-    cout << primes.size() << endl;
 }
-
-void solve() { sieve(); }
 
 int32_t main() {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int t = 1;
-    // cin >> t;
+    cin >> t;
     for (int i = 1; i <= t; i++) {
-        cout << "Case " << i << ": " << endl;
+        // cout << "Case " << i << ": " << endl;
         solve();
     }
     return 0;

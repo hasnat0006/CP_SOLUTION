65678ae6addac7c90900f29c5f6535ec66279d3f
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2024-09-19 00:36:57 +0600

    DATE: 19 September 2024

--- temp.cpp
+++ temp.cpp
@@ -1,14 +1,14 @@
 //!-----------------------------------------------------!//
 //!              Author: YUSUF REZA HASNAT              !//
-//!             Created: 17|09|2024 21:24:48            !//
+//!             Created: 18|09|2024 02:41:13            !//
 //!-----------------------------------------------------!//
 
 #pragma GCC optimize("O3")
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-#define int   long long
+#define int long long
 #define float long double
 #define vf(v) (v).begin(), (v).end()
 #define vr(v) (v).rbegin(), (v).rend()
@@ -17,38 +17,34 @@ using namespace std;
 const int mod = 1e9 + 7;
 const int inf = 1e18;
 
-const int MAX = 1000005;
-int factor[1000005];
-
-void primeFactorSeive() {
-    for (int i = 2; i <= MAX; i++) {
-        if (factor[i] == 0) {
-            for (int j = i; j <= MAX; j += i) {
-                if (factor[j] == 0)
-                    factor[j] = i;
-            }
-        }
-    }
-}
-
 void solve() {
-    for(int i = 0; i < 10; i++) {
-        cout << factor[i] << " ";
-    }
-    cout << endl;
+    int n, k;
+    cin >> n >> k;
 
-    int n;
-    cin >> n;
-    
+    auto rangeSum = [&](int l, int r) {
+        return (r * (r + 1) / 2) - (l * (l - 1) / 2);
+    };
+
+    int low = k, high = k + n - 1, ans = inf;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int sum1 = rangeSum(k, mid);
+        int sum2 = rangeSum(mid + 1, k + n - 1);
+        ans = min(ans, abs(sum1 - sum2));
+        if (sum1 > sum2)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    cout << ans << endl;
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int t = 1;
-    primeFactorSeive();
     cin >> t;
-    for (int i = 1; i <= t; i++){
-        solve(); 
+    for (int i = 1; i <= t; i++) {
+        solve();
     }
     return 0;
 }

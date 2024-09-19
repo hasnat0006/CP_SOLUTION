ead0f2aaf1f5bd8b7f32f011efd7006bcf5eaca4
Yusuf Reza Hasnat <118974654+hasnat0006@users.noreply.github.com>
2024-09-19 12:51:56 +0600

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,105 +1,68 @@
-
-clc;
-%time parameter
-fs=1000;
-t=linspace(0,1,1000);   %time vector
-    
-%message signal parameter
-fm=10; % message signal freq
-Am=1; % message signal amp
-message_signal= Am*sin(2*pi*fm.*t);
-
-
-%carrier signal parameter
-fc=100; %carrier signal freq
-Ac=2; %carrier signal amp
-carrier_signal=Ac*sin(2*pi*fc.*t);
-kf=25;
-%modulated signal
-modulated_signal=Ac*sin(2 * pi * fc .* t + kf * message_signal);
-
-
-%plotting
+fs = 10000;
+fm = 100;
+t = 0:1/fs:1000/fs;
+x = 5 * sin(2 * pi * 100 * t);
 figure;
-subplot(3,1,1);
-plot(t,message_signal);
-title('Message signal');
-xlabel('Time(s)');
-ylabel('Amplitude');
-grid on;
-subplot(3,1,2);
-plot(t,carrier_signal);
-title('Carrier signal');
-xlabel('Time(s)');
-ylabel('Amplitude');
-grid on;
+subplot(2,1,1);
+plot(t, x);
+title('Signal')
+
+
+y = [0];
+xr = 0;
+level = 0.4;
+
+
+for i = 1:length(x) - 1
+    if xr(i) <= x(i)
+        d = 1;
+        xr(i + 1) = xr(i) + level;
+    else
+        d = 0;
+        xr(i + 1) = xr(i) - level;
+    end
+    y = [y d];
+end
+stairs(t, xr);
+title('StairCase approximation signal')
+
+
+MES = sum((x - xr).^2) / length(x);
+figure;
+%delta modulation
+subplot(3,1,1)
+plot(t, y);
+title('Delta modulated signal')
+
+%delta demodulation
+subplot(3,1,2)
+y_demod = [0];
+xr_demod = 0;
+
+
+for i = 2:length(y)
+if y(i) == 1
+    xr_demod = xr_demod + level;
+else
+    xr_demod = xr_demod - level;
+end
+    y_demod = [y_demod xr_demod];
+end
+
+plot(t, y_demod);
+title('Delta demulated signal before filtering');
+
+
+%applaying low pass filter
+filter_oder = 20;
+lowpass_filter = fir1(filter_oder, (fm*2)/(fs/2), 'low');
+filter_demod_signal = filter(lowpass_filter, 1, y_demod);
+
 subplot(3,1,3);
-plot(t,modulated_signal);
-title('Modulated signal');
-xlabel('Time(s)');
-ylabel('Amplitude');
-grid on;
+plot(t, filter_demod_signal);
+title('Delta demodulated signal after filtering');
 
 
-%demodulation
-demodulated_signal=[diff(modulated_signal) 0]*fs;
-
-%1.rectification
-rectified_signal=abs(demodulated_signal);
-figure;
-subplot(2,1,1);
-plot(t,modulated_signal);
-title('Modulated signal');
-xlabel('Time(s)');
-ylabel('Amplitude');
-grid on;
-subplot(2,1,2);
-plot(t,rectified_signal);
-title('Rectified signal');
-xlabel('Time(s)');
-ylabel('Amplitude');
-grid on;
-
-%2.low pass filter
-%low pass filter parameter
-cutoff_freq=20;
-%design
-order=4;  %filter order
-nyquist_freq=fs/2;
-normalized_cutoff_freq=cutoff_freq/nyquist_freq;
-[b,a]=butter(order,normalized_cutoff_freq,'low');
-
-%apply the low pass filter to the rectified signal
-filtered_signal=filtfilt(b,a,rectified_signal);
-
-figure;
-subplot(2,1,1);
-plot(t,rectified_signal);
-title('Rectified signal');
-xlabel('Time(s)');
-ylabel('Amplitude');
-grid on;
-subplot(2,1,2);
-plot(t,filtered_signal);
-title('Filtered signal');
-xlabel('Time(s)');
-ylabel('Amplitude');
-grid on;
-
-
-figure;
-subplot(2,1,1);
-plot(t,demodulated_signal);
-title('Demodulated signal');
-xlabel('Time(s)');
-ylabel('Amplitude');
-grid on;
-subplot(2,1,2);
-plot(t,filtered_signal);
-title('Filtered signal');
-xlabel('Time(s)');
-ylabel('Amplitude');
-grid on;
 
 
 

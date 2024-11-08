fcd1d066b3346c431b7d535581e0eefaab7e7aa1
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2024-11-09 00:00:02 +0600

    DATE: 09 November 2024

--- temp.cpp
+++ temp.cpp
@@ -1,34 +1,17 @@
 //!-----------------------------------------------------!//
 //!              Author: YUSUF REZA HASNAT              !//
-//!             Created: 07|11|2024 09:09:55            !//
+//!             Created: 08|11|2024 00:34:53            !//
 //!-----------------------------------------------------!//
 
 #pragma GCC optimize("O3")
 #include <bits/stdc++.h>
-
 #ifndef ONLINE_JUDGE
 #include "D:\Documents\debug.h"
-#else 
+#else
 #define dbg(x...)
 #define dbgc(x...)
 #endif
-
 using namespace std;
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template <typename T>
-using o_set =
-    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename T>
-using o_set_g = tree<T, null_type, greater<T>, rb_tree_tag,
-                     tree_order_statistics_node_update>;
-template <typename T>
-using o_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag,
-                        tree_order_statistics_node_update>;
-template <typename T>
-using o_multiset_g = tree<T, null_type, greater_equal<T>, rb_tree_tag,
-                          tree_order_statistics_node_update>;
 
 #define ll long long
 #define vf(v) (v).begin(), (v).end()
@@ -37,60 +20,71 @@ using o_multiset_g = tree<T, null_type, greater_equal<T>, rb_tree_tag,
 const int mod = 1e9 + 7;
 const ll inf = 1e18;
 
-ll n, k;
-vector<ll> a;
-set<vector<int>> s;
-
-void f(int i, vector<int> &v) {
-    if (i == 0) {
-        s.insert(v);
-        return;
-    }
-    for (int num = 1; num <= k; num++) {
-        v.push_back(num);
-        f(i - 1, v);
-        v.pop_back();
-    }
-}
-
 void solve() {
-    cin >> n >> k;
-    a.resize(n);
-    for (ll &i : a)
-        cin >> i;
-    int zero = 0;
+    int n, q;
+    cin >> n >> q;
+    set<int> st[n];
     for (int i = 0; i < n; i++) {
-        if (a[i] == 0) {
-            zero++;
+        int x;
+        cin >> x;
+        while (x--) {
+            int y;
+            cin >> y;
+            st[i].insert(y);
         }
+        dbg(st[i]);
     }
-    vector<int> v;
-    f(zero, v);
-    int ans = 0;
-    for (auto it : s) {
-        o_multiset<int> o;
-        int possible_ans = 0;
-        for (int i = 0; i < n; i++) {
-            // find the number of elements greater than a[i]
-            int x = a[i];
-            if (x == 0) {
-                x = it.back();
-                it.pop_back();
+
+    vector<int> mex(n);
+
+    auto findMex = [&](int id, int cur) {
+        dbg("in", st[id]);
+        while (st[id].count(cur)) {
+            st[id].erase(st[id].find(cur));
+            cur++;
+        }
+        dbg("out", st[id]);
+        return cur;
+    };
+
+    for (int i = 0; i < n; i++)
+        mex[i] = findMex(i, 1);
+
+    dbg(mex);
+    while (q--) {
+        int type;
+        cin >> type;
+        if (type == 1) {
+            int x, y;
+            cin >> x >> y;
+            x--, y--;
+            if ((int)st[x].size() < (int)st[y].size()) {
+                st[x].swap(st[y]);
+                swap(mex[x], mex[y]);
             }
-            int cnt = o.order_of_key(x + 1);
-            possible_ans += (o.size() - cnt);
-            o.insert(x);
+            for (auto &it : st[y])
+                st[x].insert(it);
+            st[y].clear();
+            mex[x] = findMex(x, max(mex[x], mex[y]));
+            mex[y] = 0;
         }
-        dbgc(possible_ans);
-        ans = max(ans, possible_ans);
+        else if (type == 2) {
+            int x;
+            cin >> x;
+            x--;
+            if (st[x].empty())
+                cout << "complete" << '\n';
+            else
+                cout << mex[x] << '\n';
+        }
+        dbg(mex);
     }
-    cout << ans << '\n';
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int t = 1;
-    // cin >> t;
+    cin >> t;
     for (int i = 1; i <= t; i++) {
         solve();
     }
--- test.cpp
+++ test.cpp
@@ -1,79 +1,57 @@
 //!-----------------------------------------------------!//
 //!              Author: YUSUF REZA HASNAT              !//
-//!             Created: 07|11|2024 12:33:32            !//
+//!             Created: 08|11|2024 16:03:16            !//
 //!-----------------------------------------------------!//
 
 #pragma GCC optimize("O3")
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll   long long
+#define ll long long
 #define vf(v) (v).begin(), (v).end()
 #define vr(v) (v).rbegin(), (v).rend()
 
-const ll mod = 1e9 + 7;
+const int mod = 1e9 + 7;
 const ll inf = 1e18;
 
-const ll N = 1e7 + 5;
-ll spf[N];
-void smallestPrimeFactorUsingSeive() {
-    for (ll i = 2; i < N; i++) {
-        if (spf[i] == 0) {
-            for (ll j = i; j < N; j += i) {
-                if (spf[j] == 0)
-                    spf[j] = i;
-            }
-        }
-    }
-}
-
-ll calculate(ll l, ll r, ll x){
-    vector<ll> v;
-    while(x != 1){
-        ll temp = spf[x];
-        v.push_back(temp);
-        while(x % temp == 0)
-            x /= temp;
-    }
-    ll ans = r - l + 1;
-    for(ll mask = 1; mask < (1 << v.size()); mask++){
-        ll cnt = 0, mul = 1;
-        for(ll i = 0; i < v.size(); i++){
-            if(mask & (1 << i)){
-                cnt++;
-                mul *= v[i];
-            }
-        }
-        if(cnt & 1)
-            ans -= r / mul - (l - 1) / mul;
-        else
-            ans += r / mul - (l - 1) / mul;
-    }
-    return ans;
-}
-
+map<vector<int>, int> mp;
 void solve() {
-    ll a, b, c, d;
-    cin >> a >> b >> c >> d;
-    ll ans1 = 0;
-    for(ll i = a; i <= b; i++){
-        ans1 += calculate(c, d, i);
+    vector<int> temp;
+    for (int i = 0; i < 3; i++) {
+        int x;
+        cin >> x;
+        temp.push_back(x);
     }
-    ll ans2 = 0;
-    for(ll i = c; i <= d; i++){
-        ans2 += calculate(a, b, i);
-    }
-    cout << max(ans1, ans2) << '\n';
+    sort(vr(temp));
+    if (mp[temp] != 0)
+        cout << "perfectus\n";
+    else
+        cout << "invalidum\n";
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int t = 1;
-    smallestPrimeFactorUsingSeive();
-    //cin >> t;
+    vector<int> v(3);
+    v[0] = 6, v[1] = 1, v[2] = 1;
+    mp[v] = 1;
+    v[0] = 6, v[1] = 3, v[2] = 0;
+    mp[v] = 1;
+    v[0] = 4, v[1] = 3, v[2] = 1;
+    mp[v] = 1;
+    v[0] = 4, v[1] = 4, v[2] = 0;
+    mp[v] = 1;
+    v[0] = 4, v[1] = 2, v[2] = 1;
+    mp[v] = 1;
+    v[0] = 3, v[1] = 3, v[2] = 3;
+    mp[v] = 1;
+    v[0] = 2, v[1] = 2, v[2] = 2;
+    mp[v] = 1;
+
+    cin >> t;
     for (int i = 1; i <= t; i++) {
-        solve(); 
+        cout << "Case " << i << ": ";
+        solve();
     }
     return 0;
 }

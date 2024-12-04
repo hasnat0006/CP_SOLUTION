14e434a7c0aacb52fc5f93eef7cc552dc78e7b38
Yusuf Reza Hasnat <202214112@student.mist.ac.bd>
2024-12-05 00:00:02 +0600

    DATE: 05 December 2024

--- temp.cpp
+++ temp.cpp
@@ -5,96 +5,65 @@
 #pragma GCC optimize("O3")
 #include <bits/stdc++.h>
 #ifndef ONLINE_JUDGE
-#include "test.cpp"
+#include "D:\Documents\debug1.cpp"
 #else
-#define debug(x...)
+#define dbg(x...)
 #endif
 using namespace std;
 
-#define ll long long
+#define ll int
 #define vf(v) (v).begin(), (v).end()
 #define vr(v) (v).rbegin(), (v).rend()
 
 const ll mod = 1e9 + 7;
-const ll inf = 1e18;
-
-// query O(logn)
-// preprocessing O(n)
-struct LCA {
-    vector<ll> hi, euler, first, st;
-    vector<bool> visit;
-    ll n;
-    LCA(vector<vector<ll>> &adj, ll root = 0) {
-        n = adj.size();
-        hi.resize(n);
-        first.resize(n);
-        euler.reserve(n * 2);
-        visit.assign(n, false);
-        dfs(adj, root);
-        ll m = euler.size();
-        st.resize(m * 4);
-        build(1, 0, m - 1);
-    }
-    void dfs(vector<vector<ll>> &adj, ll node, ll h = 0) {
-        visit[node] = true;
-        hi[node] = h;
-        first[node] = euler.size();
-        euler.push_back(node);
-        for (auto to : adj[node]) {
-            if (!visit[to]) {
-                dfs(adj, to, h + 1);
-                euler.push_back(node);
-            }
-        }
-    }
-    void build(ll node, ll b, ll e) {
-        if (b == e) {
-            st[node] = euler[b];
-        }
-        else {
-            ll mid = (b + e) / 2;
-            build(node << 1, b, mid);
-            build(node << 1 | 1, mid + 1, e);
-            ll l = st[node << 1], r = st[node << 1 | 1];
-            st[node] = (hi[l] < hi[r]) ? l : r;
-        }
-    }
-    ll query(ll node, ll b, ll e, ll L, ll R) {
-        if (b > R || e < L)
-            return -1;
-        if (b >= L && e <= R)
-            return st[node];
-        ll mid = (b + e) >> 1;
-        ll left = query(node << 1, b, mid, L, R);
-        ll right = query(node << 1 | 1, mid + 1, e,  L, R);
-        if (left == -1)
-            return right;
-        if (right == -1)
-            return left;
-        return hi[left] < hi[right] ? left : right;
-    }
-    ll lca(ll u, ll v) {
-        ll left = first[u], right = first[v];
-        if (left > right)
-            swap(left, right);
-        return query(1, 0, euler.size() - 1, left, right);
-    }
-};
+const ll inf = mod;
 
 void solve() {
-    int n;
-    cin >> n;
-    vector<ll> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-        debug(i, v);
+    ll n;
+    string s, p;
+    vector<ll> a, b;
+    cin >> n >> s >> p;
+    map<char, ll> id;
+    s = "#" + s;
+    p = "#" + p;
+    a.clear(), b.clear();
+    a.resize(s.size()), b.resize(s.size());
+    for (int i = 0; i < p.size(); i++) {
+        id[p[i]] = i;
     }
+    string temp = "#";
+    for (int i = 1; i < s.size(); i++) {
+        ll index = 27 - id[s[i]];
+        temp.push_back(p[index]);
+    }
+    for (int i = 1; i < s.size(); i++) {
+        a[i] = s[i] - 'a' + 1;
+        b[i] = temp[i] - 'a' + 1;
+    }
+    ll dp[s.size()][30];
+    memset(dp, -1, sizeof(dp));
+    function<ll(ll, ll)> findMinCost = [&] (int i, int last) {
+        if (i == 0)
+            return 0;
+        if (dp[i][last] != -1)
+            return dp[i][last];
+        ll ans1 = inf, ans2 = inf;
+        if (a[i] <= last)
+            ans1 = findMinCost(i - 1, a[i]);
+        if (b[i] <= last)
+            ans2 = 1 + findMinCost(i - 1, b[i]);
+        return dp[i][last] = min(ans1, ans2);
+    };
+
+    ll ans = findMinCost(s.size() - 1, 28);
+    cout << (ans == mod ? -1 : ans) << '\n';
+
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int t = 1;
-    // cin >> t;
+    cin >> t;
     for (int i = 1; i <= t; i++) {
         solve();
     }
--- test.cpp
+++ test.cpp
@@ -1,81 +1,31 @@
+//!-----------------------------------------------------!//
+//!              Author: YUSUF REZA HASNAT              !//
+//!-----------------------------------------------------!//
+
+#pragma GCC optimize("O3")
 #include<bits/stdc++.h>
+
 using namespace std;
 
-template <typename A, typename B> string to_string(pair<A, B> p);
+#define ll long long
+#define vf(v) (v).begin(), (v).end()
+#define vr(v) (v).rbegin(), (v).rend()
 
-template <typename A, typename B, typename C> string to_string(tuple<A, B, C> p);
+const ll mod = 1e9 + 7;
+const ll inf = 1e18;
 
-template <typename A, typename B, typename C, typename D> string to_string(tuple<A, B, C, D> p);
+void solve() {
+    int n;
+    cin >> n;
+    cout << n + 10 << endl;
+}
 
-string to_string(const string& s) { return '"' + s + '"'; }
-
-string to_string(const char* s) { return to_string((string)s); }
-
-string to_string(bool b) { return (b ? "true" : "false"); }
-
-string to_string(vector<bool> v) {
-    bool first = true;
-    string res = "{";
-    for (int i = 0; i < static_cast<int>(v.size()); i++) {
-        if (!first) {
-            res += ", ";
-        }
-        first = false;
-        res += to_string(v[i]);
+int32_t main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve();
     }
-    res += "}";
-    return res;
+    return 0;
 }
-
-template <size_t N>
-string to_string(bitset<N> v) {
-    string res = "";
-    for (size_t i = 0; i < N; i++) {
-        res += static_cast<char>('0' + v[i]);
-    }
-    return res;
-}
-
-template <typename A> string to_string(A v) {
-    bool first = true;
-    string res = "{";
-    for (const auto& x : v) {
-        if (!first) {
-            res += ", ";
-        }
-        first = false;
-        res += to_string(x);
-    }
-    res += "}";
-    return res;
-}
-
-template <typename A, typename B> string to_string(pair<A, B> p) {
-    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
-}
-
-template <typename A, typename B, typename C>
-string to_string(tuple<A, B, C> p) {
-    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " +
-           to_string(get<2>(p)) + ")";
-}
-
-template <typename A, typename B, typename C, typename D>
-string to_string(tuple<A, B, C, D> p) {
-    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " +
-           to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
-}
-
-void debug_out() { cerr << endl; }
-
-template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T) {
-    cerr << " " << to_string(H);
-    debug_out(T...);
-}
-
-#ifdef LOCAL
-#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
-#else
-#define debug(...)
-#endif
